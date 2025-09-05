7ea1fd4d23b472944a1b50fbe78a2027b346955e
Mujtaba Abid <mujtabaabid06@gmail.com>
2025-09-05 15:29:19 +0500

    POSTGRES

--- config/db.js
+++ config/db.js
@@ -1,27 +1,42 @@
-import { Sequelize } from "sequelize";
+// import { Sequelize } from "sequelize";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,       // database name
+//   process.env.DB_USER,       // username
+//   process.env.DB_PASS,       // password
+//   {
+//     host: process.env.DB_HOST,   // e.g. localhost
+//     dialect: "mysql",
+//     logging: false,              // true karo agar queries console me dekhni ho
+//   }
+// );
+
+// // test connection
+// const connectDB = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log("✅ Database connected with Sequelize");
+//   } catch (error) {
+//     console.error("❌ Database connection failed:", error.message);
+//   }
+// };
+
+// export { sequelize, connectDB };
+
 import dotenv from "dotenv";
+import { Pool } from "pg";
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,       // database name
-  process.env.DB_USER,       // username
-  process.env.DB_PASS,       // password
-  {
-    host: process.env.DB_HOST,   // e.g. localhost
-    dialect: "mysql",
-    logging: false,              // true karo agar queries console me dekhni ho
-  }
-);
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
 
-// test connection
-const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("✅ Database connected with Sequelize");
-  } catch (error) {
-    console.error("❌ Database connection failed:", error.message);
-  }
-};
-
-export { sequelize, connectDB };
+export default pool;

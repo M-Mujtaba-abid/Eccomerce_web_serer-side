def555a2ded2a8f24e8ad83be8e6affee9567160
Mujtaba Abid <mujtabaabid06@gmail.com>
2025-09-05 15:51:04 +0500

    updated db.js

--- config/db.js
+++ config/db.js
@@ -29,23 +29,26 @@
 // config/db.js
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 export const sequelize = new Sequelize(
-  process.env.DB_NAME,     // your Postgres database name
-  process.env.DB_USER,     // your DB user
-  process.env.DB_PASS,     // your DB password
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "postgres",
-    logging: false, // optional, set true to see SQL queries
+    logging: false,
   }
 );
 
-// Optional: Test connection
-sequelize.authenticate()
-  .then(() => console.log("Postgres + Sequelize connected successfully"))
-  .catch(err => console.error("Unable to connect to the database:", err));
-l;
+// Export connectDB for index.js
+export const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Postgres + Sequelize connected successfully");
+  } catch (err) {
+    console.error("Unable to connect to the database:", err);
+  }
+};

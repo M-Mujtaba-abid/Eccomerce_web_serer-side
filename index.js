725dcdfad2b2374a3ed04e4e902e49c95301dd32
Mujtaba Abid <mujtabaabid06@gmail.com>
2025-09-08 13:02:54 +0500

    set index.js again back

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
-// import dotenv from "dotenv"
-// import app from "./app.js"
-// import { connectDB } from "./config/db.js";
-// dotenv.config()
-
-
-//  const port=process.env.PORT
-
-// connectDB();
-
-// app.listen(port,()=>{
-//     console.log("server is running on port ",port)
-// })
-
-
-// index.js
-import dotenv from "dotenv";
-import app from "./app.js";
+import dotenv from "dotenv"
+import app from "./app.js"
 import { connectDB } from "./config/db.js";
+dotenv.config()
 
-dotenv.config();
 
-// Database connect
+ const port=process.env.PORT
+
 connectDB();
 
-// Local vs Production handling
-if (process.env.NODE_ENV !== "production") {
-  const port = process.env.PORT || 8000;
-  app.listen(port, () => {
-    console.log(`✅ Server running locally on port ${port}`);
-  });
-}
-
-// Vercel ke liye `export default app` zaroori hai
-export default app;
+app.listen(port,()=>{
+    console.log("server is running on port ",port)
+})
